3cc01c214e12b28f94212378638bd45bcc47299e
Arugaf <arug113@gmail.com>
2022-10-23 05:18:28 +0300

    assets pack and character movement controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerMovement : MonoBehaviour {
+    public CharacterController controller;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Transform followingCamera;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    public float turnSmoothVelocity;
+
+    void Update() {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        var direction = new Vector3(horizontal, 0f, vertical).normalized;
+        var isometricDirection = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0)).MultiplyPoint3x4(direction);
+
+        if (direction.magnitude >= 0.1f) {
+            var targetAngle = Mathf.Atan2(isometricDirection.x, isometricDirection.z) * Mathf.Rad2Deg;
+            var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,
+                turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(isometricDirection * speed * Time.deltaTime);
+        }
+
+        followingCamera.position = transform.position;
     }
 }
